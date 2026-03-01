5ff68e2e57b6d5e6b5b8b17b32599f1708e95b7d
Shorook Khaled <shorookkhaled559@gmail.com>
2026-03-01 16:32:47 +0200

    feat(footer): add footer

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
 import React from 'react'
 
@@ -6,6 +7,7 @@ export default function About() {
     <>
         <NavBar />
         <div>Section About</div>
+        <Footer />
     </>
   )
 }
--- app/(public)/feature/page.tsx
+++ app/(public)/feature/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
 import React from 'react'
 
@@ -6,6 +7,8 @@ export default function Feature() {
     <>
         <NavBar />
         <div>Section Feature</div>
+        <Footer />
+    
     </>
   )
 }
